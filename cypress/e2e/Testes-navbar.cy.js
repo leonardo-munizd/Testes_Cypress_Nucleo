d27d629acc4cb1ce830092a68aec7f68aff07924
leonardoddev <leonardo.devicari2@gmail.com>
2025-07-29 20:14:54 -0300

    melhorando testes

--- cypress/e2e/Testes-navbar.cy.js
+++ cypress/e2e/Testes-navbar.cy.js
@@ -9,13 +9,9 @@ describe("Testar os componentes da barra de navegação", () => {
     cy.visit("");
     cy.get(
       ".elementor-element-388ec9a9 > .elementor-element > .elementor-widget-container > .elementor-button-wrapper > .elementor-button"
-    ).click();
-    cy.get(
-      ".elementor-element-388ec9a9 > .elementor-element > .elementor-widget-container > .elementor-button-wrapper > .elementor-button"
     ).should(
       "have.attr",
       "href",
-      "include",
       "https://api.whatsapp.com/send/?phone=5554996391889&text=Ol%C3%A1%2C+vim+atrav%C3%A9s+do+site+e+gostaria+de+mais+informa%C3%A7%C3%B5es.+Podem+me+ajudar%3F&type=phone_number&app_absent=0"
     );
   });
